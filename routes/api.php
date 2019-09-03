a2e75041d63f1cf2cd4155e2c004ba63ca440896
David Stinghen <davidstinghen@gmail.com>
2019-09-02 21:12:19 -0300

    feature: add  registerand login methods

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', 'Auth\RegisterController@register');
+
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('restaurants', 'RestaurantController@index');
+    Route::get('restaurants/{restaurant}', 'RestaurantController@show');
+    Route::post('restaurants', 'RestaurantController@store');
+    Route::put('restaurants/{restaurant}', 'RestaurantController@update');
+    Route::delete('restaurants/{restaurant}', 'RestaurantController@delete');
 });
